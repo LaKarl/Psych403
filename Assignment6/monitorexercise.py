08674bf1fc913dcd3420511fd3a9eadbdf8f39a1
Karl La <113373163+LaKarl@users.noreply.github.com>
2022-11-21 01:18:02 -0700

    Update monitorexercise.py

--- Assignment6/monitorexercise.py
+++ Assignment6/monitorexercise.py
@@ -7,4 +7,4 @@ mon = monitors.Monitor('myMonitor', width = 35.56, distance = 60)
 mon = setSizePix([1600,900])
 mon.save()
 #-define the window (size, color, units, fullscreen mode) using psychopy functions
-win = visual.Window(monitor=mon, size=(800,800), color=(-1,-1,-1), units ='None', fullscr =False, useRetina=True)
+win = visual.Window(monitor=mon, size=(800,800), color=(-1,-1,-1), units ='None', fullscr =False,)
